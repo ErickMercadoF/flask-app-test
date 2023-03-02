d115d82bb8ddae5ebc6f7cb5e4158c2877326291
Erick Mercado <erick911@gmail.com>
2023-03-02 19:18:38 -0300

    Commit con coneccion flask
    falta el dockerfile

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return 'Hello Erick, puedes avanzar'
+  return 'Hello Erick, puedes avanzar, y esta todo en git'
 
 if __name__ == '__main__'
   app.run(host='localhost', port=8080, debug=True)
