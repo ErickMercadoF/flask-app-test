8055b9fbe2b1e20b97f643b0b8bbc580febc22ed
Erick Mercado <erick911@gmail.com>
2023-03-02 20:51:43 -0300

    cambios app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-
+import os
 
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return 'Hello Erick, puedes avanzar, y esta todo en git'
+@app.route("/")
+def hello_world():
+    name = os.environ.get("NAME", "World")
+    return "Hello {}!".format(name)
 
-if __name__ == '__main__'
-  app.run(host='localhost', port=8080, debug=True)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
